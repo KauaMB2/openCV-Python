c5d7e18f30419e091f4628f65edcc67a27e5e537
Kauã Moreira Batista <95621020+KauaMB2@users.noreply.github.com>
2022-06-05 21:52:36 -0300

    Delete openCV directory

--- openCV/1 - Reading images and videos/readingImages.py
+++
@@ -1,4 +0,0 @@
-import cv2 as cv
-img=cv.imread('Resources/Photos/cat_large.jpg')
-cv.imshow('Cat',img)
-cv.waitKey(0)
--- openCV/1 - Reading images and videos/readingVideos.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-capture=cv.VideoCapture('Resources/Videos/dog.mp4')
-while True:
-    isTrue,frame=capture.read()
-    cv.imshow('Video',frame)
-    if cv.waitKey(20) & 0xFF==ord('d'):#Se eu pressionar a tecla 'd', o vídeo encerra
-        break
-capture.release()#Encerra o vídeo
-cv.destroyAllWindows()#Fecha todas as janelas abertas
